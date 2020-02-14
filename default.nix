3fa72192932ffb7d7094c53f3310487bcd2e4820
Sean Hickman <sean@grumply.com>
2020-02-13 23:33:13 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-cond, pure-prop, pure-portal, pure-theme, pure-css, pure-lifted, stdenv }:
 mkDerivation {
   pname = "pure-modal";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-cond pure-prop pure-portal pure-theme pure-css pure-lifted ];
   homepage = "github.com/grumply/pure-modal";
